106cb01d062c25e7e26106721dae4bea39f5510f
bkaneb <bryankaneb2@gmail.com>
2021-10-30 16:09:28 +0200

    correction card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,7 +7,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions } from '@mui/material';
+import { CardActionArea } from '@mui/material';
 
 export default function Cards(props) {
   // eslint-disable-next-line react/prop-types
