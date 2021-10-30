e9779d975e786c3f6b327281e152f459c3ac402a
bkaneb <bryankaneb2@gmail.com>
2021-10-30 15:52:28 +0200

    correction lintter

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,121 @@
+/* eslint-disable no-else-return */
+/* eslint-disable no-shadow */
+/* eslint-disable react/button-has-type */
 import './App.css';
 import React from 'react';
-import Cards from './components/Cards';
 import axios from 'axios';
-let i =1;
-let apiFilter = "https://rawg.io/api/games?key=1ff4a83b9ad54d31a63835f56db9e88d&page=1&page_size=10";
+import Cards from './components/Cards';
 
-/*filtrer par platerforme parent : &parent_platforms=4*/
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []//permettera de stocker la réponse de l'API
-    };
-  }
- 
-   componentDidMount() {
-    axios 
-    .get(apiFilter)//requête
-      .then(
-        (res) => {
-          //permet de transmettre à items la réponse de l'API grâce à "setState"
-          console.log(res.data);
-          this.setState({
-            isLoaded: true,
-            items : res.data.results
-          });
-        },
-        // Remarque : il est important de traiter les erreurs ici
-        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
-        // des exceptions provenant de réels bugs du composant.
-        (error) => {
-          this.setState({
-            isLoaded: true,//permet de dire que l'application est entrain de charger l'API
-            error
-          });
-        }
-      )
-  } 
-  componentDidUpdate() {
-     apiFilter = "https://rawg.io/api/games?key=1ff4a83b9ad54d31a63835f56db9e88d&page=" + i;
-     axios 
-     .get(apiFilter)//requête
-       .then(
-         (res) => {
-           //permet de transmettre à items la réponse de l'API grâce à "setState"
-           console.log(res.data);
-           this.setState({
-             isLoaded: true,
-             items : res.data.results
-           });
-         },
-         // Remarque : il est important de traiter les erreurs ici
-         // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
-         // des exceptions provenant de réels bugs du composant.
-         (error) => {
-           this.setState({
-             isLoaded: true,//permet de dire que l'application est entrain de charger l'API
-             error
-           });
-         }
-       )
-    }
-  render() {
-    const { error, isLoaded, items } = this.state;//Pour ne plus utiliser "this." par la suite
-    console.log(items);
-    if (error) {//si erreur on affiche laquel
-      return <div>Erreur : {error.message}</div>;
-    } else if (!isLoaded) {//si ca charge on affiche "chargement..."
-      return <div style={{textAlign:"center"}}>Chargement…</div>;
+let page = 1;
+/* filtrer par platerforme parent : &parent_platforms=4 */
+function App() {
+  const [pages, setPage] = React.useState(1);
+  const [pageChanges, setPageChanges] = React.useState(true); // true = suivant et false = precedent
+  const [apiFilter, setApiFilter] = React.useState(
+    `https://rawg.io/api/games?key=a9d50f2881ee441fbaf3e0368a2f3589&page=1`
+  );
+  const [error, setError] = React.useState(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+  React.useEffect(() => {
+    console.log(pages);
+    console.log(pageChanges);
+    // setApiFilter(`https://rawg.io/api/games?key=a9d50f2881ee441fbaf3e0368a2f3589`);
+    if (pageChanges) {
+      axios
+        .get(apiFilter) // requête
+        .then(
+          (res) => {
+            // permet de transmettre à items la réponse de l'API grâce à "setState"
+            console.log(res.data);
+            setIsLoaded(true);
+            setItems(res.data.results);
+            setApiFilter(res.data);
+          },
+          // Remarque : il est important de traiter les erreurs ici
+          // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+          // des exceptions provenant de réels bugs du composant.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
     } else {
-      return (
-           <ul className='grid-parent'>
-          {/*on va lire le tableau de la réponse parametre par parametre*/}
-          {items.map(item => (
-             <li key={item.id} style={{listStyle:'none'}} className='grid-enfant'>
-               {/*A chaque lecture de parametre on crée une nouvelle carte en fonction du parametre(jeux)*/}
-              <Cards image={item.background_image} 
-              name={item.name} 
-              released={item.released}
-              genres={item.genres}  
-              platformes={item.parent_platforms}         
-              />
-            </li> 
-          ))}
-          <a href="#">
-          <button onClick={()=> i++}> plus i </button>
-          <button onClick={()=> i--}> moins i </button>
-          </a>
-          </ul>
-      );
+      axios
+        .get(apiFilter) // requête
+        .then(
+          (res) => {
+            // permet de transmettre à items la réponse de l'API grâce à "setState"
+            console.log(res.data);
+            setIsLoaded(true);
+            setItems(res.data.results);
+            setApiFilter(res.data);
+          },
+          // Remarque : il est important de traiter les erreurs ici
+          // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+          // des exceptions provenant de réels bugs du composant.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
     }
+  }, [pages]);
+
+  if (error) {
+    // si erreur on affiche laquel
+    return <div>Erreur : {error.message}</div>;
+  } else if (!isLoaded) {
+    // si ca charge on affiche "chargement..."
+    return (
+      <div style={{ textAlign: 'center' }}>Chargement de Games Library…</div>
+    );
+  } else {
+    return (
+      <ul className="grid-parent">
+        {/* on va lire le tableau de la réponse parametre par parametre */}
+        {items.map((item) => (
+          <li
+            key={item.id}
+            style={{ listStyle: 'none' }}
+            className="grid-enfant"
+          >
+            {/* A chaque lecture de parametre on crée une nouvelle carte en fonction du parametre(jeux) */}
+            <Cards
+              image={item.background_image}
+              name={item.name}
+              released={item.released}
+              genres={item.genres}
+              platformes={item.parent_platforms}
+            />
+          </li>
+        ))}
+        <a href="/#">
+          <button
+            onClick={() => {
+              setPageChanges(true);
+              setApiFilter(apiFilter.next);
+              page += 1;
+              setPage(page);
+            }}
+          >
+            {' '}
+            Page suivante{' '}
+          </button>
+          <button
+            onClick={() => {
+              setPageChanges(false);
+              setApiFilter(apiFilter.previous);
+              page -= 1;
+              setPage(page);
+            }}
+          >
+            {' '}
+            Page precedente{' '}
+          </button>
+        </a>
+      </ul>
+    );
   }
 }
 
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,43 +1,52 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-return-assign */
 import * as React from 'react';
-import "../App.css"
+import * as PropTypes from 'prop-types';
+import '../App.css';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { CardActionArea, CardActions } from '@mui/material';
 
 export default function Cards(props) {
-    const {image, name, released, genres, platformes} = props;
-    let strGenres = "";//permettra de stocker les noms des genres
-    genres.forEach((genre) => ( strGenres += genre.name + ", "));//Stock dans la variable "strGenres" les genres
-    let str = strGenres.substring(0, strGenres.length -1);
-    let setPlatformes = "";//permettra de stocker les noms des platefromes
-    platformes.forEach((platforme) => ( setPlatformes += platforme.platform.name + ", "));
+  // eslint-disable-next-line react/prop-types
+  const { image, name, released, genres, platformes } = props;
+  let strGenres = ''; // permettra de stocker les noms des genres
+  // eslint-disable-next-line react/prop-types
+  genres.forEach((genre) => (strGenres += `${genre.name}, `)); // Stock dans la variable "strGenres" les genres
+  const str = strGenres.substring(0, strGenres.length - 1);
+  let setPlatformes = ''; // permettra de stocker les noms des platefromes
+  platformes.forEach(
+    (platforme) => (setPlatformes += `${platforme.platform.name}, `)
+  );
   return (
-    <Card className='card'>
-         {/* framework mui */}
+    <Card className="card">
+      {/* framework mui */}
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="200"
-          image={image}
-          alt={name}
-        />
+        <CardMedia component="img" height="200" image={image} alt={name} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {name}{/* affiche le nom du jeux transmis */}
+            {name}
+            {/* affiche le nom du jeux transmis */}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-          Plateformes: {setPlatformes}<br/>
-          Date: {released}<br/>
-          Genres: {str}
+            Plateformes: {setPlatformes}
+            <br />
+            Date: {released}
+            <br />
+            Genres: {str}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions sx={{display:"flex",
-        justifyContent:"center"
-        }}>
-      </CardActions>
+      <CardActions sx={{ display: 'flex', justifyContent: 'center' }} />
     </Card>
   );
 }
+Cards.protoTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  released: PropTypes.string,
+  genres: PropTypes.array,
+  platformes: PropTypes.array,
+};
